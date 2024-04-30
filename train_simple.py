0c5e0afdce7524072146b6b2038adfe9deb9daa7
elezovic-natalia <159857826+elezovic-natalia@users.noreply.github.com>
2024-04-30 12:55:03 -0500

    Update train_simple.py

--- train_simple.py
+++ train_simple.py
@@ -1,4 +1,4 @@
-import json
+iimport json
 import os
 import random
 import subprocess
@@ -144,7 +144,7 @@ def get_config_foldername(config: dict) -> str:
 def main(
     batch_size: int = 32,
     max_ctx: int = 1024,
-    ds_name: str = "paws_labeled_final",
+    ds_name: str = "ethics_justice",
     loss: str = "xent",
     n_docs: int = 20000,
     n_test_docs: int = 10000,
