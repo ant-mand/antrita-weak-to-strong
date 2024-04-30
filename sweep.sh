69296a35b980ea4655f786e77d5b73f89d4198ce
Antrita Manduva <amanduva@uchicago.edu>
2024-04-29 23:47:35 -0500

    strong path

--- sweep.sh
+++ sweep.sh
@@ -6,10 +6,11 @@ BATCH_SIZE=32
 LEARNING_RATE=1e-5
 EPOCHS=3
 SWEEP_SUBFOLDER="boolq_experiment_01"
-LOSS_TYPE="logconf"
+LOSS_TYPE="xent"
 MODEL_CKPT="gpt2-boolq"
-STRONG_CKPT_PATH="./results/gpt2-medium/bs=32-dn=boolq-e=3-l=xent-l=1e-05-ms=gpt2-medium/model.safetensors"
-WEAK_LABELS_PATH="./results/gpt2/bs=32-dn=boolq-e=3-l=xent-l=1e-05-ms=gpt2/weak_labels"
+STRONG_CKPT_PATH="./results/$SWEEP_SUBFOLDER/bs=$BATCH_SIZE-dn=$DATASET_NAME-e=$EPOCHS-l=$LOSS_TYPE-l=$LEARNING_RATE-mc=$MODEL_CKPT"
+# STRONG_CKPT_PATH="./results/gpt2-medium/bs=32-dn=boolq-e=3-l=xent-l=1e-05-ms=gpt2-medium/model.safetensors"
+# WEAK_LABELS_PATH="./results/gpt2/bs=32-dn=boolq-e=3-l=xent-l=1e-05-ms=gpt2/weak_labels"
 
 python train_simple.py \
     --model_size=$MODEL_SIZE \
