f570306d6e166c5b2f5a27de1ba9492c5b7e9955
Antrita Manduva <amanduva@uchicago.edu>
2024-04-30 07:01:52 -0500

    trying

--- sweep.sh
+++ sweep.sh
@@ -8,7 +8,7 @@ EPOCHS=3
 SWEEP_SUBFOLDER="boolq_experiment_01/subtask_checkpoint"
 LOSS_TYPE="xent"
 MODEL_CKPT="gpt2-boolq"
-STRONG_CKPT_PATH="./results/$SWEEP_SUBFOLDER/bs=$BATCH_SIZE-dn=$DATASET_NAME-e=$EPOCHS-l=$LOSS_TYPE-l=$LEARNING_RATE-mc=$MODEL_CKPT/model.safetensors"
+STRONG_CKPT_PATH="./results/$SWEEP_SUBFOLDER/bs=$BATCH_SIZE-dn=$DATASET_NAME-e=$EPOCHS-l=$LOSS_TYPE-l=$LEARNING_RATE-mc=$MODEL_CKPT"
 WEAK_LABELS_PATH="./results/$SWEEP_SUBFOLDER/bs=$BATCH_SIZE-dn=$DATASET_NAME-e=$EPOCHS-l=$LOSS_TYPE-l=$LEARNING_RATE-mc=$MODEL_CKPT/weak_labels"
 # STRONG_CKPT_PATH="./results/gpt2-medium/bs=32-dn=boolq-e=3-l=xent-l=1e-05-ms=gpt2-medium/model.safetensors"
 # WEAK_LABELS_PATH="./results/gpt2/bs=32-dn=boolq-e=3-l=xent-l=1e-05-ms=gpt2/weak_labels"
